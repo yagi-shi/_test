108dfe1990d44bfe91c69813d2af012df6e39184
yagi-shi <yukisoccer.kk@gmail.com>
2022-01-29 20:13:40 +0900

    change database seeder

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+   <meta charset="UTF=8">
+   <title>Blog</title>
+</head>
+    <body>
+        <form action="/posts/"{{ $post->id}} method="POST">
+            @csrf
+            @method('PUT')
+            <div class='content_title'>
+            <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{{ $post->title }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="お疲れ様ー－－ー－－">{{ $post->body }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            </div>
+             <input type="submit" value="保存"/>
+        </form>
+    </body>
+</html>
