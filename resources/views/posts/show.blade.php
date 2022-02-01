2130b069fe8681dedef913924d6a155451fa6615
yagi-shi <yukisoccer.kk@gmail.com>
2022-02-01 11:10:20 +0900

    08-6

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,11 @@
    <body>
         <h1 class="title">{{ $post->title }}</h1>
         <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p> 
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+    @csrf
+    @method('DELETE')
+    <button type="submit">delete</button> 
+</form>
         <div class="content">
             <div class="content__post">
                 <h3>なんやこれ</h3>
@@ -16,5 +21,11 @@
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <script>
+        function DELETE();confirm('本当に消去しますか?')
+        if ( function DELETE )
+            {
+            console.log('消去しました')};
+        </script>
     </body>
 </html>
